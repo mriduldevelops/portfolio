4a417610ae7633aa1dda2e346ffebcd662c5f698
Mridul <sharmamridul045@gmail.com>
2024-12-02 17:04:47 +0530

    text changed to string

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About() {
         <h2 className='text-5xl font-semibold font-serif tracking-wider'>ABOUT</h2>
         <hr className='border-4 border-black mt-5 w-20' />
       </div>
-      <p className='mt-10 text-2xl text-center'>Hi, I&apos;m a frontend developer from India with a passion for crafting responsive and interactive web experiences. For the past year, I&apos;ve been honing my skills at Appopoleis Studios, where I&apos;ve worked on creating sleek landing pages and robust e-commerce websites. Whether its building user-friendly interfaces or bringing ideas to life with clean, modern designs, I thrive on delivering solutions that look great and work flawlessly. Let&apos;s create something amazing together!</p>
+      <p className='mt-10 text-2xl text-center'>{"Hi, I&apos;m a frontend developer from India with a passion for crafting responsive and interactive web experiences. For the past year, I&apos;ve been honing my skills at Appopoleis Studios, where I&apos;ve worked on creating sleek landing pages and robust e-commerce websites. Whether its building user-friendly interfaces or bringing ideas to life with clean, modern designs, I thrive on delivering solutions that look great and work flawlessly. Let&apos;s create something amazing together!"}</p>
       <div className='mt-10 flex gap-4 justify-center flex-wrap'>
         <div className='bg-[#f2f2f2] border-4 border-[#444444] px-4 py-2'>
           <p className='text-lg md:text-xl'>Name: <span className='font-semibold'>Mridul Sharma</span></p>
